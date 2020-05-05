5b98cb83d8c0bea866e87255eff983c7e6d471db
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-05-05 23:04:39 +0300

    add some progress with parsing a file

--- src/main/java/com/codeminders/counter/FileCounter.java
+++ src/main/java/com/codeminders/counter/FileCounter.java
@@ -5,6 +5,9 @@ import com.codeminders.model.CountLinesResult;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * @author Nazar Lelyak.
@@ -12,46 +15,62 @@ import java.io.IOException;
 public class FileCounter implements LinesCounter {
 
     private final String filePath;
-    private final CountLinesResult countLinesResult;
+    private CountLinesResult countLinesResult;
 
-    private enum Comment { // todo consider another format for checking commented code
-        BLOCK_BEGGING("/*"),
-        BLOCK_ENDING("*/"),
-        LINE_COMMENT("//")
-        ;
-
-        private String symbol;
-
-        Comment(String symbol) {
-            this.symbol = symbol;
-        }
-
-        public String getSymbol() {
-            return symbol;
-        }
-
-        private boolean validate(String line) {
-            return line.startsWith(this.getSymbol());
-        }
-    }
+    private Pattern blockCommentRegex = Pattern.compile("/\\*/*(?s:(?!\\*/).)*\\*/");
 
     public FileCounter(String path) {
         this.filePath = path;
-        this.countLinesResult = new CountLinesResult(path);
+//        this.countLinesResult = new CountLinesResult(path);
     }
 
     @Override
     public CountLinesResult countLines() {
-        // todo consider how to count all that comments here
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filePath)))) {
+            int counter = 0;
+            boolean isBlockComment = false;
 
-            while (reader.readLine() != null) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine().trim();
 
+                if (line.startsWith("/*")) {
+                    isBlockComment = true;
+                    continue;
+                }
+
+                if (line.contains("*/") && isBlockComment) {
+                    line = line.substring(line.indexOf("*/"));
+
+                    // remove all group comments for a line
+                    line = line.replaceAll("/\\*/*(?s:(?!\\*/).)*\\*/", "");
+                    isBlockComment = false;
+                }
+
+                if (line.contains("//") && !isBlockComment) {
+                    line = line.substring(0, line.indexOf("//"));
+                }
+
+                if (!line.isEmpty() && !isBlockComment) {
+                    counter += 1;
+                }
             }
-
+            countLinesResult = CountLinesResult.of(filePath, counter);
         } catch (IOException e) {
             System.err.println("Exception during processing file: " + filePath);
         }
-        return this.countLinesResult;
+
+        return countLinesResult;
+    }
+
+    public static void main(String[] args) {
+
+        String[] array = {"/home/nazar/IdeaProjects/Companies/Codeminders/code-lines-counter-java/src/test/resources/3_code_lines.java", "/home/nazar/IdeaProjects/Companies/Codeminders/code-lines-counter-java/src/test/resources/5_code_lines.java"};
+
+        for (String a : array) {
+            FileCounter fileCounter = new FileCounter(a);
+            CountLinesResult result = fileCounter.countLines();
+            System.out.println(String.format("For file: %s - total count: %s%n", a, result.getTotalLinesCount()));
+        }
+
     }
 }
--- src/main/java/com/codeminders/model/CountLinesResult.java
+++ src/main/java/com/codeminders/model/CountLinesResult.java
@@ -16,4 +16,26 @@ public class CountLinesResult {
     public CountLinesResult(String fileName) {
         this.fileName = fileName;
     }
+
+    public CountLinesResult(String fileName, int totalLinesCount, List<CountLinesResult> children) {
+        this.fileName = fileName;
+        this.totalLinesCount = totalLinesCount;
+        this.children = children;
+    }
+
+    public static CountLinesResult of(String fileName, int total) {
+        return new CountLinesResult(fileName, total, null);
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getTotalLinesCount() {
+        return totalLinesCount;
+    }
+
+    public List<CountLinesResult> getChildren() {
+        return children;
+    }
 }
